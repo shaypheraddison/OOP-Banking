04b4a32ded4c2ab9d0b54a1b3e4879be9d397c67
shaypheraddison <shaypherashe@gmail.com>
2024-09-29 22:06:11 -0500

    beginning Savings class

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 #simple banking app for cli
 #! /usr/bin/python
 class BankAccount:
-    def __init__(self, balance, allow_both_transactions):
+    def __init__(self, balance):
         self.balance = balance
-        self.allow_both_transactions = allow_both_transactions
+        
+    def view_balance(self):
+        print(f"Your current balance is: ${self.balance}")
 
     
 class Checking(BankAccount):
+
     def add_to_balance(self):
-        deposit = int(input("How much money would you like to deposit? \n$"))
+        deposit = int(input("How much money would you like to deposit? $"))
         self.balance += deposit
+        print(f"${deposit} has been added to your balance.")
         return self.balance
 
     def remove_from_balance(self):
-        withdraw = int(input("How much would you like to take out of your balance? \n$"))
+        withdraw = int(input("How much would you like to take out of your balance? $"))
         if withdraw > self.balance:
             print("There is not enough money to withdraw, please deposit more first.")
         else:
             self.balance -= withdraw
-        
+            print(f"${withdraw} has been removed from your balance.")
+
         return self.balance
-        
-    def view_balance(self):
-        print(self.balance)
+
+class Saving(BankAccount):
+    
+    pass
     
 
-c1 = Checking(0, True)
+starting_bal = int(input("Please set your starting balance: $"))
+c1 = Checking(starting_bal)
 c1.add_to_balance()
 c1.view_balance()
 c1.remove_from_balance()

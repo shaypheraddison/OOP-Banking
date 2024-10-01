d8c5c6cc3c5421233c68ab15706c0e92181d6ed5
shaypheraddison <shaypherashe@gmail.com>
2024-09-30 23:55:31 -0500

    added class for Savings account and began writing the CLI program to interact with the banking app

--- main.py
+++ main.py
@@ -3,37 +3,70 @@
 class BankAccount:
     def __init__(self, balance):
         self.balance = balance
-        
-    def view_balance(self):
-        print(f"Your current balance is: ${self.balance}")
-
-    
-class Checking(BankAccount):
 
     def add_to_balance(self):
         deposit = int(input("How much money would you like to deposit? $"))
         self.balance += deposit
-        print(f"${deposit} has been added to your balance.")
+        print(f"${deposit} has been added to your balance. New balance is {self.balance}")
         return self.balance
+
+    def view_balance(self):
+        print(f"Your current balance is: ${self.balance} \n")
+
+    
+class Checking(BankAccount):
+    def __init__(self, balance, has_overdraft_protection):
+        super().__init__(balance)
+        self.has_overdraft_protection = has_overdraft_protection
 
     def remove_from_balance(self):
         withdraw = int(input("How much would you like to take out of your balance? $"))
-        if withdraw > self.balance:
-            print("There is not enough money to withdraw, please deposit more first.")
+        if withdraw > self.balance and self.has_overdraft_protection == False:
+            print("There is not enough money to withdraw, please deposit more first. Don't forget to sign up for Overdraft Protection too!")
         else:
             self.balance -= withdraw
             print(f"${withdraw} has been removed from your balance.")
 
         return self.balance
 
-class Saving(BankAccount):
-    
-    pass
-    
 
-starting_bal = int(input("Please set your starting balance: $"))
-c1 = Checking(starting_bal)
-c1.add_to_balance()
-c1.view_balance()
-c1.remove_from_balance()
-c1.view_balance()
+
+class Saving(BankAccount):
+    interest_rate = 1.04
+
+    def apply_interest(self):
+        years_open = int(input("How many years has this account been open? "))
+        if years_open == 0:
+            print("No interest has been accrued just yet, come back later")
+            return self.balance
+        else:
+            new_balance = (years_open * Saving.interest_rate) * self.balance
+            print(f"Interest applied to account! New Savings balance is: {new_balance:.2f}")
+
+        return new_balance
+    
+    
+while True: 
+    print("1. Checking Account \n2. Savings Account \n3. Exit")
+    account_type = int(input("Please select an account type: "))
+
+    if account_type == 1:
+        starting_balance = int(input("How much will your starting balance be? $"))
+        checking = Checking(starting_balance, True)
+
+        while True: 
+            print("1. Check Balance \n2. Deposit \n3. Withdraw \n4. Exit")
+            choice_1 = int(input("Please select an option: "))
+            if choice_1 == 1:
+                checking.view_balance()
+            elif choice_1 == 2:
+                checking.add_to_balance()
+            elif choice_1 == 3:
+                checking.remove_from_balance(True)
+            elif choice_1 == 4:
+                print("Closing out of app now. Thank you!")
+                break
+            else:
+                print("Please select a correct option.")
+
+
